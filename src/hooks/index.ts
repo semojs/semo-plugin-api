7f9486d028b22caf65c80fa9522a442ca233180e
Richard Yu <vipzhicheng@gmail.com>
2020-07-19 23:59:18 +0800

    fix: adjust repl hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,6 +3,6 @@ import { api } from '../common/api'
 export = async (Utils) => {
   return {
     hook_repl: new Utils.Hook('semo', { api: api('semo-repl') }),
-    hook_component: new Utils.Hook('semo', { api: api('semo-component') }),
+    hook_component: new Utils.Hook('semo', { api }),
   }
 }
