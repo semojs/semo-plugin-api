99d0405de7d622230426596795f922717330c72d
Richard Yu <vipzhicheng@gmail.com>
2020-07-19 21:48:44 +0800

    fix: hook repl api object

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,8 +1,9 @@
 import { api } from '../common/api'
 
 export = async (Utils) => {
+  const API = api('semo-repl')
   return {
-    hook_repl: new Utils.Hook('semo', { api }),
-    hook_component: new Utils.Hook('semo', { api }),
+    hook_repl: new Utils.Hook('semo', { api: API }),
+    hook_component: new Utils.Hook('semo', { api: API }),
   }
 }
